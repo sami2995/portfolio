4feeafcb71a09597475965b812dc2061f2a636f8
SAMIYAS SOLOMON <138712866+sami2995@users.noreply.github.com>
2025-01-08 20:10:08 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,2 @@
-document.querySelector('form').addEventListener('submit', function (e) {
-    e.preventDefault();
-    alert('Thank you for reaching out! I will get back to you soon.');
-  });
+
   
